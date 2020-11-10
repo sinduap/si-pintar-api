451ef71ffe4b1cdc0c0e5b9159cbfc342f83ce95
Sindu Andita <sinduandita@gmail.com>
2020-11-10 22:48:10 +0700

    si-pintar-api back-end using express and node js

---
+++ controllers/signin.js
@@ -0,0 +1,29 @@
+const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Incorrect form submission");
+  }
+  db.select("email", "hash")
+    .where("email", "=", email)
+    .from("login")
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("Unable to get user"));
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("Wrong credentials"));
+};
+
+module.exports = {
+  handleSignin: handleSignin,
+};
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "hero",
+    database: "si_pintar",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => console.log(data));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3001, () => {
+  console.log("App is running on port 3001");
+});
+
+/*
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/user:id --> GET = user
+/image --> PUT --> user
+
+*/
